fba1979a882d79aa89f831a82e562d7ec347f0dd
Edward3635 <Edgard3635@gmail.com>
2024-03-03 13:09:00 +0200

    moved to another folder

--- jwt-auth/client/src/types/authResponse.tsx
+++
@@ -1,7 +0,0 @@
-import { IUser } from './user'
-
-export interface IAuthResponse {
-	accessToken: string
-	refreshToken: string
-	user: IUser
-}
--- jwt-auth/client/src/types/formInitialVal.ts
+++
@@ -1,4 +0,0 @@
-export interface IFormInitVal {
-	email: string
-	password: string
-}
